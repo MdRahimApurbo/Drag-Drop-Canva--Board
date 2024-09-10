b7ec9ce1d6dad9b73e0ca677d4ae82ab9593b898
Md Rahim Apurbo <engr.mdapurbo@gmail.com>
2024-09-10 08:14:11 +0600

    cd

--- src/components/CanvaBoard.tsx
+++ src/components/CanvaBoard.tsx
@@ -1,33 +1,73 @@
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { createPortal } from "react-dom";
 import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
 
 const CanvaBoard = () => {
   const [columns, setColumns] = useState<Column[]>([]);
-  console.log(columns);
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+  const [activeColumn, setActiveColumn] = useState<Column>();
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    })
+  );
   return (
     <div className="m-auto flex min-h-screen w-full items-center  overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto">
-        <div className="flex gap-4">
-          {columns.map((col) => (
-            <ColumnContainer
-              key={col.id}
-              column={col}
-              deleteColumn={deleteColumn}
-            />
-          ))}
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((col) => (
+                <ColumnContainer
+                  key={col.id}
+                  column={col}
+                  deleteColumn={deleteColumn}
+                  updateColumn={updateColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => {
+              createColumn();
+            }}
+            className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackground border-2 border-columnBackground p-4 ring-rose-400 hover:ring-2 flex gap-2 items-center"
+          >
+            <Plus size={20} />
+            Add Column
+          </button>
         </div>
-        <button
-          onClick={() => {
-            createColumn();
-          }}
-          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackground border-2 border-columnBackground p-4 ring-rose-400 hover:ring-2 flex gap-2 items-center"
-        >
-          <Plus size={20} />
-          Add Column
-        </button>
-      </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+                updateColumn={updateColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     </div>
   );
 
@@ -41,6 +81,38 @@ const CanvaBoard = () => {
   function deleteColumn(id: Id) {
     const filterColumns = columns.filter((col) => col.id !== id);
     setColumns(filterColumns);
+  }
+  function updateColumn(id: Id, title: string) {
+    setColumns((columns) => {
+      const columnToUpdate = columns.find((col) => col.id === id);
+      if (!columnToUpdate) return columns;
+      columnToUpdate.title = title;
+      return [...columns];
+    });
+  }
+  function onDragStart(event: DragStartEvent) {
+    // console.log(event);
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  }
+  function onDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    console.log(active, over);
+    if (!over) return;
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+    if (activeColumnId === overColumnId) return;
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (col) => col.id === activeColumnId
+      );
+      const overColumnIndex = columns.findIndex(
+        (col) => col.id === overColumnId
+      );
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
   }
   function generateId() {
     return Math.floor(Math.random() * 10001);
--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,16 +1,59 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { Trash2 } from "lucide-react";
+import { useState } from "react";
 import { Column, Id } from "../types";
 
 interface ColumnContainerProps {
   column: Column;
   deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
 }
 
 const ColumnContainer = (props: ColumnContainerProps) => {
-  const { column, deleteColumn } = props;
-  return (
-    <div className="bg-columnBackground w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+  const { column, deleteColumn, updateColumn } = props;
+  const [editMode, setEditMode] = useState(false);
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  if (isDragging) {
+    return (
       <div
+        ref={setNodeRef}
+        style={style}
+        className="bg-columnBackground opacity-40 border-2 border-rose-500 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col "
+      />
+    );
+  }
+
+  return (
+    <div
+      className="bg-columnBackground w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col"
+      ref={setNodeRef}
+      style={style}
+    >
+      <div
+        {...attributes}
+        {...listeners}
+        onClick={() => {
+          setEditMode(true);
+        }}
         className="bg-mainBackground text-md h-[60px] cursor-grab rounded-none rounded-b-none p-3 font-bold border-columnBackground border-4 flex justify-between items-center"
         key={column.id}
       >
@@ -18,7 +61,24 @@ const ColumnContainer = (props: ColumnContainerProps) => {
           <div className="flex justify-between items-center bg-columnBackground px-2 py-1 text-sm">
             0
           </div>
-          {column.title}
+          {!editMode ? (
+            column.title
+          ) : (
+            <input
+              className="bg-black focus:border-rose-500 border-rounded outline-none px-2"
+              value={column.title}
+              onChange={(e) => updateColumn(column.id, e.target.value)}
+              autoFocus
+              onBlur={() => {
+                setEditMode(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  setEditMode(false);
+                }
+              }}
+            />
+          )}
         </div>
         <button
           onClick={() => {
